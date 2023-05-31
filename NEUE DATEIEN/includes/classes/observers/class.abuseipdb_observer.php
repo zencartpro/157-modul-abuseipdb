a79ad04d3a13fb4d527f93ef7608b9bcd225b49a
Jörg Schönberger <office@webchills.at>
2023-05-31 11:24:27 +0200

    Version 2.0.8

--- NEUE DATEIEN/includes/classes/observers/class.abuseipdb_observer.php
+++ NEUE DATEIEN/includes/classes/observers/class.abuseipdb_observer.php
@@ -5,7 +5,7 @@
  * Copyright 2023 marcopolo
  * see https://github.com/CcMarc/AbuseIPDB
  * License: GNU General Public License (GPL)
- * version $Id: class.abuseipdb_observer.php 2023-05-30 21:31:16Z webchills $
+ * version $Id: class.abuseipdb_observer.php 2023-05-31 10:02:16Z webchills $
  */
 
 class abuseipdb_observer extends base {
@@ -96,11 +96,34 @@ class abuseipdb_observer extends base {
                 return;
             }
 
-            // Check if the IP is manually blocked
-            if (in_array($ip, $blocked_ips)) {
-                if ($debug_mode == true) {
-                    error_log('IP ' . $ip . ' blocked from cache');
-                }
+			// Define the path to your blacklist file, and if it exists, load its content into the $file_blocked_ips array
+			$file_blocked_ips = array();
+			if (defined('ABUSEIPDB_BLACKLIST_ENABLE') && ABUSEIPDB_BLACKLIST_ENABLE && defined('ABUSEIPDB_BLACKLIST_FILE_PATH') && file_exists(ABUSEIPDB_BLACKLIST_FILE_PATH)) {
+			$file_blocked_ips = file(ABUSEIPDB_BLACKLIST_FILE_PATH, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			}
+			
+			// Check if the IP is manually blocked
+			$ip_blocked = false;
+			
+			// First, check in the blocked_ips array
+			if (in_array($ip, $blocked_ips)) {
+			$ip_blocked = true;
+			}
+
+			// If the IP is not found in the array, check in the file, only if ABUSEIPDB_BLACKLIST_ENABLE is true
+			if (!$ip_blocked && defined('ABUSEIPDB_BLACKLIST_ENABLE') && ABUSEIPDB_BLACKLIST_ENABLE) {
+				foreach ($file_blocked_ips as $blocked_ip) {
+					if (strpos($ip, $blocked_ip) === 0) { // if the current IP starts with the blocked IP
+						$ip_blocked = true;
+						break;
+					}
+				}
+			}
+
+			if ($ip_blocked) {
+				if ($debug_mode == true) {
+					error_log('IP ' . $ip . ' blocked from cache');
+			}
 
                 $log_file_name = 'abuseipdb_blocked_' . date('Y_m') . '.log';
                 $log_file_path = ABUSEIPDB_LOG_FILE_PATH . $log_file_name;
